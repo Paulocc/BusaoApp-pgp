93b0e01f65c8d4779f37d8cc4ca5bb10e88325d5
BeKrein <bernardokrein@gmail.com>
2024-04-09 21:56:05 -0300

    primeiro draft do cadastro de viagens

--- lib/pages/cadastrar_viagens.dart
+++ lib/pages/cadastrar_viagens.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/text_form_field_padrao.dart';
+
 class CadastrarViagens extends StatefulWidget {
   const CadastrarViagens({super.key, required this.title});
   final String title;
@@ -11,14 +13,69 @@ class CadastrarViagens extends StatefulWidget {
 class _CadastrarViagensState extends State<CadastrarViagens> {
   @override
   Widget build(BuildContext context) {
+    Size sizeOf = MediaQuery.of(context).size;
+
     return Scaffold(
-      body: Padding( padding: EdgeInsets.only(top: 80),
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 32), 
-          child: Container(
-          child: TextFormField(),
-        ),
+      appBar: AppBar(
+        title: Text('Cadastra de Viagem'),
       ),
+      body: SingleChildScrollView(
+        child: Container(
+          height: sizeOf.height * .9,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32),
+                child: Column(
+                  children: [
+                    TextFormFieldPadrao(
+                      titulo: 'Titulo',
+                      subTitulo: 'Digite o Titulo da viagem...',
+                    ),
+                    SizedBox(height: 16),
+                    TextFormFieldPadrao(
+                      titulo: 'Local Saida',
+                      subTitulo: 'Digite o local da saida..',
+                    ),
+                    SizedBox(height: 16),
+                    TextFormFieldPadrao(
+                      titulo: 'Horario de saida',
+                      subTitulo: 'Digite o horario da saida...',
+                    ),
+                    SizedBox(height: 16),
+                    TextFormFieldPadrao(
+                      titulo: 'Local Retorno',
+                      subTitulo: 'Digite o local do retorno...',
+                    ),
+                    SizedBox(height: 16),
+                    TextFormFieldPadrao(
+                      titulo: 'Horario de retorno',
+                      subTitulo: 'Digite o horario da retorno...',
+                    ),
+                    SizedBox(height: 16),
+                    TextFormFieldPadrao(
+                      titulo: 'Dias de viagem',
+                      subTitulo: 'Digite os dias que acontecerar a viagem...',
+                    ),
+                    SizedBox(height: 16),
+                    TextFormFieldPadrao(
+                      titulo: 'Horiaro disparo',
+                      subTitulo: 'Digite o horario de diparo dos lembretes...',
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 64),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Salvar'),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/pages/cadastro_passageiro.dart
+++ lib/pages/cadastro_passageiro.dart
@@ -10,21 +10,60 @@ class CadastroPassageiro extends StatefulWidget {
 }
 
 class _CadastroPassageiroState extends State<CadastroPassageiro> {
-  int _counter = 0;
+  //Nome, embarque, desembarque, celular, cpf, email
+  TextEditingController nomeController = TextEditingController();
+  TextEditingController embarqueController = TextEditingController();
+  TextEditingController desembarqueController = TextEditingController();
+  TextEditingController celularController = TextEditingController();
+  TextEditingController cpfController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void _salvar() {
+    print('Se apareceu no vscode é sucesso');
+    print(nomeController.text);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          color: Colors.blue,
-          child: TextFormField(),
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            TextFormField(
+              controller: nomeController,
+              decoration: InputDecoration(labelText: 'Nome'),
+            ),
+            TextFormField(
+              controller: embarqueController,
+              decoration: InputDecoration(labelText: 'Endereço de embarque'),
+            ),
+            TextFormField(
+              controller: desembarqueController,
+              decoration: InputDecoration(labelText: 'Endereço de desembarque'),
+            ),
+            TextFormField(
+              controller: celularController,
+              decoration: InputDecoration(labelText: 'Celular'),
+            ),
+            TextFormField(
+              controller: cpfController,
+              decoration: InputDecoration(labelText: 'CPF'),
+            ),
+            TextFormField(
+              controller: emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _salvar,
+              child: Text('Salvar'),
+            ),
+          ],
         ),
       ),
     );
